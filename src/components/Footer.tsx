480394db53a895e53136d280d7239474255da857
stilldaniel <ogundipe_inioluwa@yahoo.com>
2024-12-31 14:14:28 +0100

    Massimo

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
+            <Link href="/" className="font-bold text-xl">MASSIMO</Link>
+            <p>&copy; ALL RIGHTS RESERVED</p>
+        </div>
+    )
+}
+export default Footer
